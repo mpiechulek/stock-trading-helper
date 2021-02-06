72f3e55b20da80b6e69d6cc398fc7cd9a65fe9f1
MP <47081376+mpiechulek@users.noreply.github.com>
2021-02-06 15:01:50 +0100

    added logic to local storage

--- src/app/core/services/language.service.ts
+++ src/app/core/services/language.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LanguageService {
 
+  private chosenLanguage: string = 'en';
+  chosenLanguage$ = new Subject<string>();
+
   constructor() { }
+
+  addToLocalStorage(language: string){
+    localStorage.setItem('language', language);  
+  }
+
+  getFormLocalStorage() {
+    
+    if(localStorage.hasOwnProperty('language')) {
+      this.chosenLanguage = localStorage.getItem('language');
+
+    } else {
+      this.chosenLanguage = 'en';
+      this.addToLocalStorage(this.chosenLanguage);
+
+    }
+
+    this.chosenLanguage$.next(this.chosenLanguage);
+  }
+
+  getCurrentLanguage(): string{
+    return this.chosenLanguage;
+  }
 }
