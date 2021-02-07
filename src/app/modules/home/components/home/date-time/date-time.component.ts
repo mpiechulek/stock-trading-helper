5e1ea61d83291f51cec87f1e4e9e9c4326b6ed5a
MP <47081376+mpiechulek@users.noreply.github.com>
2021-02-07 10:07:32 +0100

    created date time cmpoent

---
+++ src/app/modules/home/components/home/date-time/date-time.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-date-time',
+  templateUrl: './date-time.component.html'
+})
+export class DateTimeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
