0bca6fd1b8e704b54f197616e0b695a483339700
MP <47081376+mpiechulek@users.noreply.github.com>
2021-02-07 11:08:43 +0100

    date time works

--- src/app/modules/home/components/home/date-time/date-time.component.ts
+++ src/app/modules/home/components/home/date-time/date-time.component.ts
@@ -6,9 +6,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DateTimeComponent implements OnInit {
 
+  dayNames: string[] = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday"
+  ];
+
+  dateData: Date;
+  time: string;
+  date: string;
+  dayOfTheWeek: string;
+
   constructor() { }
 
   ngOnInit(): void {
+
+    this.dateData = new Date();
+    this.date = this.dateData.toLocaleDateString();   
+    this.dayOfTheWeek = this.dayNames[(this.dateData.getDate()) - 1];
+
+    this.calculateCurrentTime();
   }
 
+  calculateCurrentTime() {
+    setInterval(() => {
+      this.dateData = new Date();
+      this.time = this.dateData.toLocaleTimeString();      
+    }, 1000);
+  }
 }
