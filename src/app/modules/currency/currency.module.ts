8dbf10baf7f21f2cedc239137bf9b5b4f71c63e9
MP <47081376+mpiechulek@users.noreply.github.com>
2021-02-08 14:15:17 +0100

    configured currency module

--- src/app/modules/currency/currency.module.ts
+++ src/app/modules/currency/currency.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CurrencyRoutingModule } from './currency-routing.module';
+import { CurrencyContainerComponent } from './page/currency/currency.container';
+import { CurrencyComponent } from './components/currency/currency.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CurrencyContainerComponent,
+    CurrencyComponent
+  ],
   imports: [
     CommonModule,
     CurrencyRoutingModule
