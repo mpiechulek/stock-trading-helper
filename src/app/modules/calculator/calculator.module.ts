b5ec12b460798b8a795f2eae17a08b59ff3b07eb
MP <47081376+mpiechulek@users.noreply.github.com>
2021-02-08 14:03:44 +0100

    configured calculator component

--- src/app/modules/calculator/calculator.module.ts
+++ src/app/modules/calculator/calculator.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CalculatorRoutingModule } from './calculator-routing.module';
-
+import { CalculatorContainerComponent } from './page/calculator/calculator.container';
+import { CalculatorComponent } from './components/calculator/calculator.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CalculatorContainerComponent,
+    CalculatorComponent
+  ],
   imports: [
     CommonModule,
-    CalculatorRoutingModule
+    CalculatorRoutingModule,
   ]
 })
 export class CalculatorModule { }
--- src/app/modules/calculator/components/calculator/calculator.component.ts
+++ src/app/modules/calculator/components/calculator/calculator.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-calculator',
+  selector: 'app-calculator-ui',
   templateUrl: './calculator.component.html' 
 })
 export class CalculatorComponent implements OnInit {
