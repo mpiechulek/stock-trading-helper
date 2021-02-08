9d826b2775947653bcba8688cff18a0ca6b45832
MP <47081376+mpiechulek@users.noreply.github.com>
2021-02-08 13:49:28 +0100

    generated components

---
+++ src/app/modules/calculator/page/calculator/calculator.container.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-calculator',
+  templateUrl: './calculator.container.html',
+})
+ 
+export class CalculatorContainerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
