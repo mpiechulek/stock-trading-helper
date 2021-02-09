97434026789fe433d3c4c1b39128d87aa9ffefae
MP <47081376+mpiechulek@users.noreply.github.com>
2021-02-09 14:33:08 +0100

    added formControl

--- src/app/modules/trade/components/trade/trade-dialog/trade-dialog.component.ts
+++ src/app/modules/trade/components/trade/trade-dialog/trade-dialog.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-trade-dialog',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 
 export class TradeDialogComponent implements OnInit {
 
+  myForm:  FormGroup;
   constructor() { }
 
   ngOnInit(): void {
