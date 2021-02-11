d087a2d33c7db5e7b32228fe0fb3c0f7cc71afed
MP <47081376+mpiechulek@users.noreply.github.com>
2021-02-11 13:06:12 +0100

    asas

--- src/app/shared/directives/number-digit.directive.ts
+++ src/app/shared/directives/number-digit.directive.ts
@@ -10,25 +10,26 @@ export class NumberDigitDirective {
   private el: ElementRef;
   private elementEnterValue: string;
   private previousNumber: string = '';
-  private numberOfDecimalPontPlaces: number;
+  private numberOfDecimalPointPlaces: number;
+  private isBlur: boolean = false;
 
   constructor(el: ElementRef) {
     this.el = el;
   }
 
-  // @HostBinding('value') onInitValue: string;   
-
   @HostListener('keyup', ['$event']) onKeyboardEnter(event) {
     this.verifyInput(event);
   }
 
-  // @HostListener('blur', ['$event']) onBlur() {   
-  //   this.el.nativeElement.value = this.previousNumber;  
-  // }
+  @HostListener('blur', ['$event']) onBlur(event) {
+    this.isBlur = true;
+    this.verifyInput(event);
+  }
 
-  // @HostListener('focus', ['$event']) onFocus() {  
-  //   this.el.nativeElement.value = ''; 
-  // }
+  @HostListener('focus', ['$event']) onFocus() {
+    this.isBlur = false;
+    // this.el.nativeElement.value = ''; 
+  }
 
   verifyInput(event) {
 
@@ -48,15 +49,17 @@ export class NumberDigitDirective {
 
     // Checking if the input string is a positive float number or has a comma (','), because '.'
     // is taken as a number
-    if (
-      this.isPositiveFloat(this.elementEnterValue) ||
-      this.elementEnterValue.includes(',')
-    ) {
-      console.log(this.elementEnterValue);
+    if ((this.isPositiveFloat(this.elementEnterValue) ||
+      this.elementEnterValue.includes(','))) {
+
       this.toValidNumber(this.elementEnterValue);
-    } else {
-      this.el.nativeElement.value = this.previousNumber;
-    }
+      console.log('no blur');     
+      }
+      // If the input is blur and the entered numer is an integer, add '.0000'
+    // } else if (this.isBlur && this.isIntegerNumber(this.elementEnterValue)) {
+    //   console.log('blur');
+    //   this.el.nativeElement.value = this.previousNumber + '.0000';
+    // }
   }
 
   stringHasEnoughNumbersAfterDecimal(value: string): boolean {
@@ -70,7 +73,7 @@ export class NumberDigitDirective {
 
   isPositiveFloat(value: any): boolean {
     return !isNaN(value) && Number(value) >= 0;
-  }
+  } 
 
   toValidNumber(value: string) {
     let commaToDot: string;
@@ -87,15 +90,16 @@ export class NumberDigitDirective {
       this.setDecimalPoints(oneDotInString);
       number = this.toFloatNumber(oneDotInString);
       fixedNumber = this.fixToDecimalNumbers(number);
+      // Resetting the decimal places container    
       fixedToString = this.numberToString(fixedNumber);
 
       // If the entered string was '123.' or '123,', the last character was 
       // removed in the process of number fixing, we are adding it back
-      if (this.elementEnterValue[this.elementEnterValue.length - 1] === '.') {
+      if (this.elementEnterValue[this.elementEnterValue.length - 1] === '.' && !this.isBlur) {
         fixedToString = fixedToString + '.';
       }
 
-      if (this.elementEnterValue[this.elementEnterValue.length - 1] === ',') {
+      if (this.elementEnterValue[this.elementEnterValue.length - 1] === ',' && !this.isBlur) {
         fixedToString = fixedToString + '.';
       }
 
@@ -125,16 +129,21 @@ export class NumberDigitDirective {
   setDecimalPoints(value: string): void {
     const split: string[] = value.split('.');
 
+    if (this.isBlur) {
+      this.numberOfDecimalPointPlaces = 4;
+      return;
+    }
+
     if (split[1] === '') {
-      this.numberOfDecimalPontPlaces = 0;
+      this.numberOfDecimalPointPlaces = 0;
     } else if (split[1].length === 1) {
-      this.numberOfDecimalPontPlaces = 1;
+      this.numberOfDecimalPointPlaces = 1;
     } else if (split[1].length === 2) {
-      this.numberOfDecimalPontPlaces = 2;
+      this.numberOfDecimalPointPlaces = 2;
     } else if (split[1].length === 3) {
-      this.numberOfDecimalPontPlaces = 3;
+      this.numberOfDecimalPointPlaces = 3;
     } else if (split[1].length >= 4) {
-      this.numberOfDecimalPontPlaces = 4;
+      this.numberOfDecimalPointPlaces = 4;
     }
   }
 
@@ -143,7 +152,7 @@ export class NumberDigitDirective {
   }
 
   fixToDecimalNumbers(value: any | number): number {
-    return value.toFixed(this.numberOfDecimalPontPlaces);
+    return value.toFixed(this.numberOfDecimalPointPlaces);
   }
 
   numberToString(value: number): string {
