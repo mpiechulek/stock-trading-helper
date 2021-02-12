bfb64cf57aaf7f6a89c2c063f44bc0585ff4951d
MP <47081376+mpiechulek@users.noreply.github.com>
2021-02-12 10:28:37 +0100

    changed subject name in sidenav service

--- src/app/core/services/side-nav.service.ts
+++ src/app/core/services/side-nav.service.ts
@@ -9,18 +9,18 @@ export class SideNavService {
 
   private sideNav: boolean = false;
   // Here we create the Observer
-  private sideNavIsOpen$ = new Subject<boolean>();
+  private sideNavIsOpen = new Subject<boolean>();
 
   constructor() { }
 
   toggleSideNav(): void {
     this.sideNav = !this.sideNav;
     // This is informing all the subscribers about changes
-    this.sideNavIsOpen$.next(this.sideNav);
+    this.sideNavIsOpen.next(this.sideNav);
   }
 
   // We are subscribing to this
   sideNavOpen() {
-    return this.sideNavIsOpen$.asObservable();
+    return this.sideNavIsOpen.asObservable();
   }
 }
