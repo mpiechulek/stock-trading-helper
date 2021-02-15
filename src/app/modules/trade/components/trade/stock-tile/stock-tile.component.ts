a74750e805fcd17d98561a63f674d7395d7a0426
MP <47081376+mpiechulek@users.noreply.github.com>
2021-02-15 11:10:01 +0100

    changed div's to ul li tags form trade lists compoennts

--- src/app/modules/trade/components/trade/stock-tile/stock-tile.component.ts
+++ src/app/modules/trade/components/trade/stock-tile/stock-tile.component.ts
@@ -3,9 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-stock-tile',
   templateUrl: './stock-tile.component.html'
- 
+
 })
 export class StockTileComponent implements OnInit {
+
+  array = [{}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}]
 
   constructor() { }
 
