49c13cc280773000661c8b484787dd4ba072e886
MP <47081376+mpiechulek@users.noreply.github.com>
2021-02-15 14:59:32 +0100

    generated calc form component

--- src/app/modules/calculator/calculator.module.ts
+++ src/app/modules/calculator/calculator.module.ts
@@ -5,12 +5,14 @@ import { CalculatorRoutingModule } from './calculator-routing.module';
 import { CalculatorContainerComponent } from './page/calculator/calculator.container';
 import { CalculatorComponent } from './components/calculator/calculator.component';
 import { CalcDeviceComponent } from './components/calculator/calc-device/calc-device.component';
+import { CalcFormComponent } from './components/calculator/calc-form/calc-form.component';
 
 @NgModule({
   declarations: [
     CalculatorContainerComponent,
     CalculatorComponent,
-    CalcDeviceComponent
+    CalcDeviceComponent,
+    CalcFormComponent
   ],
   imports: [
     CommonModule,
---
+++ src/app/modules/calculator/components/calculator/calc-form/calc-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-calc-form',
+  templateUrl: './calc-form.component.html',
+  styleUrls: ['./calc-form.component.scss']
+})
+export class CalcFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
