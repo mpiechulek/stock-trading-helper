041acd7d83c07e14a06fb35fc27aec41da519a83
MP <47081376+mpiechulek@users.noreply.github.com>
2021-02-15 14:16:56 +0100

    generated calc-device compoent

--- src/app/modules/calculator/calculator.module.ts
+++ src/app/modules/calculator/calculator.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 import { CalculatorRoutingModule } from './calculator-routing.module';
 import { CalculatorContainerComponent } from './page/calculator/calculator.container';
 import { CalculatorComponent } from './components/calculator/calculator.component';
+import { CalcDeviceComponent } from './components/calculator/calc-device/calc-device.component';
 
 @NgModule({
   declarations: [
     CalculatorContainerComponent,
-    CalculatorComponent
+    CalculatorComponent,
+    CalcDeviceComponent
   ],
   imports: [
     CommonModule,
---
+++ src/app/modules/calculator/components/calculator/calc-device/calc-device.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-calc-device',
+  templateUrl: './calc-device.component.html'
+})
+export class CalcDeviceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
