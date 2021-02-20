99eb4fdb029ae437fc41375ea7ef58851951cc10
MP <47081376+mpiechulek@users.noreply.github.com>
2021-02-20 13:25:58 +0100

    generated stock tile presenter service

--- src/app/modules/trade/components/trade/stock-tile/stock-tile.component.ts
+++ src/app/modules/trade/components/trade/stock-tile/stock-tile.component.ts
@@ -1,10 +1,12 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FixedSizeVirtualScrollStrategy, VIRTUAL_SCROLL_STRATEGY } from '@angular/cdk/scrolling';
 import { StockTileModel } from '../../../../../data/models/stock-tile.model';
+import { StockTilePresenterService } from './stock-tile.presenter';
 
 @Component({
   selector: 'app-stock-tile',
-  templateUrl: './stock-tile.component.html'
+  templateUrl: './stock-tile.component.html',
+  providers:[StockTilePresenterService]
 
 })
 export class StockTileComponent implements OnInit {
@@ -22,9 +24,11 @@ export class StockTileComponent implements OnInit {
   @Output()
   editTile: EventEmitter<string> = new EventEmitter<string>();
 
-  constructor() { }
+  constructor(private stockTilePresenterService:StockTilePresenterService) { }
 
   ngOnInit(): void {
+
+
   }
 
   onEditTile(id:string): void {
