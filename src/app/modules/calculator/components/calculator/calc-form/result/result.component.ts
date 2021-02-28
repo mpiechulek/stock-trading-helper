888b7a6e1c6b24fde2e998eb29a8b0974e33852d
MP <47081376+mpiechulek@users.noreply.github.com>
2021-02-28 17:48:42 +0100

    created statistic components

--- src/app/modules/calculator/components/calculator/calc-form/result/result.component.ts
+++ src/app/modules/calculator/components/calculator/calc-form/result/result.component.ts
@@ -6,9 +6,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResultComponent implements OnInit {
 
+  // When 0 the expansion panel is default opened, 1 closed
+  step = 0;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  // Toggle expansion pane 
+  setStep(index: number) {
+    this.step = index;
   }
 
 }
