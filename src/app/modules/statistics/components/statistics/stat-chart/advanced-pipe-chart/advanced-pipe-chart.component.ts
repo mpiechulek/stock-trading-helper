1f1e6ffc9d72f563111817c5e1f5542835b6294f
MP <47081376+mpiechulek@users.noreply.github.com>
2021-03-02 11:50:14 +0100

    charts added

---
+++ src/app/modules/statistics/components/statistics/stat-chart/advanced-pipe-chart/advanced-pipe-chart.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { single } from './data';
+
+@Component({
+  selector: 'app-advanced-pipe-chart',
+  templateUrl: './advanced-pipe-chart.component.html'
+})
+export class AdvancedPipeChartComponent implements OnInit {
+
+  single: any[];
+  view: any[] = [700, 400];
+
+  // options
+  gradient: boolean = true;
+  showLegend: boolean = true;
+  showLabels: boolean = true;
+  isDoughnut: boolean = false;
+  animations: boolean = true;
+   
+  colorScheme = {
+    domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
+  };
+
+  constructor() {
+    Object.assign(this, { single });
+  }
+
+
+  ngOnInit(): void {
+  }
+
+
+  onSelect(data): void {
+    console.log('Item clicked', JSON.parse(JSON.stringify(data)));
+  }
+
+  onActivate(data): void {
+    console.log('Activate', JSON.parse(JSON.stringify(data)));
+  }
+
+  onDeactivate(data): void {
+    console.log('Deactivate', JSON.parse(JSON.stringify(data)));
+  }
+
+}
--- src/app/modules/statistics/components/statistics/stat-chart/stat-chart.component.ts
+++ src/app/modules/statistics/components/statistics/stat-chart/stat-chart.component.ts
@@ -3,13 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-stat-chart',
   templateUrl: './stat-chart.component.html'
- 
 })
+
 export class StatChartComponent implements OnInit {
 
-  constructor() { }
+  ngOnInit(): void { }
 
-  ngOnInit(): void {
-  }
 
 }
