db81fbd56f1200a0d1a68e3dc6ceb2c2ebd7ffb1
MP <47081376+mpiechulek@users.noreply.github.com>
2021-03-08 09:44:28 +0100

    added styling to currency

--- src/app/modules/currency/currency.module.ts
+++ src/app/modules/currency/currency.module.ts
@@ -4,16 +4,25 @@ import { CommonModule } from '@angular/common';
 import { CurrencyRoutingModule } from './currency-routing.module';
 import { CurrencyContainerComponent } from './page/currency/currency.container';
 import { CurrencyComponent } from './components/currency/currency.component';
-
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
+import { CurrencyFormComponent } from './components/currency/currency-form/currency-form.component';
 
 @NgModule({
   declarations: [
     CurrencyContainerComponent,
-    CurrencyComponent
+    CurrencyComponent,
+    CurrencyFormComponent
   ],
   imports: [
     CommonModule,
-    CurrencyRoutingModule
+    CurrencyRoutingModule,
+    MatInputModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatSelectModule
   ]
 })
 export class CurrencyModule { }
