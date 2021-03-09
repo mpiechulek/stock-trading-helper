d97c2d195a04746a5d2e03e9befd93c7eae63881
MP <47081376+mpiechulek@users.noreply.github.com>
2021-03-09 09:49:18 +0100

    createt method in currency facade

--- src/app/core/services/facades/currency.facade.ts
+++ src/app/core/services/facades/currency.facade.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { CurrencyService } from '../../../data/api/currency.service';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CurrencyFacadeService {
 
-  constructor() { }
+  constructor(private currencyService: CurrencyService) { }
+
+  /**
+   * 
+   * @param currencyName 
+   * @returns 
+   */
+  getCurrencyData(currencyName: string) {
+    return this.currencyService.getCurrencyData(currencyName)
+      .pipe(
+        tap(data => console.log(data))
+      )
+  }
+
 }
