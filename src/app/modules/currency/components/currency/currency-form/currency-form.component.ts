14cd731cd87d1a5e561dcd6e80c74627509d17e7
MP <47081376+mpiechulek@users.noreply.github.com>
2021-03-09 10:07:31 +0100

    created data pipe to child componwent in currency module

--- src/app/modules/currency/components/currency/currency-form/currency-form.component.ts
+++ src/app/modules/currency/components/currency/currency-form/currency-form.component.ts
@@ -17,4 +17,11 @@ export class CurrencyFormComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onGetCurrencyData(event) {
+
+    console.log(event);
+    
+
+  }
+
 }
--- src/app/modules/currency/components/currency/currency.component.ts
+++ src/app/modules/currency/components/currency/currency.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-currency-ui',
@@ -7,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CurrencyComponent implements OnInit {
 
- 
+  @Input() 
+  private currencyData; 
 
   constructor() { }
 
