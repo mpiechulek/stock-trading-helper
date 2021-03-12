e0e1ec34f7a68dd5ac26b6f7be76a7bbaa1c2df0
MP <47081376+mpiechulek@users.noreply.github.com>
2021-03-12 08:20:05 +0100

    aded theme changing for linear chart

--- src/app/modules/statistics/components/statistics/linear-chart/linear-chart.component.ts
+++ src/app/modules/statistics/components/statistics/linear-chart/linear-chart.component.ts
@@ -11,20 +11,20 @@ export class LinearChartComponent implements OnInit {
   view: any[] = [700, 300];
 
   // options
-  legend: boolean = true;
+  legend: boolean = false;
   showLabels: boolean = true;
   animations: boolean = true;
   xAxis: boolean = true;
   yAxis: boolean = true;
   showYAxisLabel: boolean = true;
   showXAxisLabel: boolean = true;
-  xAxisLabel: string = 'Year';
-  yAxisLabel: string = 'Population';
+  xAxisLabel: string = 'Date';
+  yAxisLabel: string = 'Ballance';
   timeline: boolean = true;
   autoScale: boolean = true;
 
   colorScheme = {
-    domain: ['#5AA454', '#E44D25', '#CFC0BB', '#7aa3e5', '#a8385d', '#aae3f5']
+    domain: ['#2196F3']
   };
 
   constructor() {
@@ -32,6 +32,9 @@ export class LinearChartComponent implements OnInit {
   }
 
   ngOnInit(): void {
+
+    console.log(new Date());
+    
     
   }
 
