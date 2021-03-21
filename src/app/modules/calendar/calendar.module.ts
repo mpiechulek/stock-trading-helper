4a3665a26910f16bfaca98c255b69802586a2987
MP <47081376+mpiechulek@users.noreply.github.com>
2021-03-21 09:50:27 +0100

    implemented angulr calendar package

--- src/app/modules/calendar/calendar.module.ts
+++ src/app/modules/calendar/calendar.module.ts
@@ -1,18 +1,31 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { MatButtonModule } from '@angular/material/button';
+
 import { CalendarRoutingModule } from './calendar-routing.module';
 import { CalendarContainerComponent } from './page/calendar/calendar.container';
 import { CalendarComponent } from './components/calendar/calendar.component';
+import { CalComponentComponent } from './components/calendar/cal-component/cal-component.component';
+import { CalendarHeaderComponent } from './components/calendar/cal-component/utils/calendar-header.component';
 
 @NgModule({
   declarations: [
     CalendarContainerComponent,
-    CalendarComponent
+    CalendarComponent,
+    CalComponentComponent,
+    CalendarHeaderComponent
   ],
   imports: [
     CommonModule,
-    CalendarRoutingModule
+    CalendarRoutingModule,
+    MatButtonModule,
+    CalendarModule.forRoot({
+      provide: DateAdapter,
+      useFactory: adapterFactory,
+    })
   ]
 })
-export class CalendarModule { }
+export class CalendarDateModule { }
