46b45a2cd838846c742e65167c599d77955cd0d7
MP <47081376+mpiechulek@users.noreply.github.com>
2021-06-18 08:12:20 +0200

    created blank methods

--- src/app/core/services/snack-bar.service.ts
+++ src/app/core/services/snack-bar.service.ts
@@ -1,15 +1,47 @@
 import { Injectable } from '@angular/core';
-import {MatSnackBar} from '@angular/material/snack-bar';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SnackBarService {
 
+  
+
   constructor(private snackBar: MatSnackBar) { }
 
-  //  //snackbar
-  //  this.snackBar.open('Message archived', 'Undo', {
-  //   duration: 2000
-  // });
+
+
+  /**
+   * 
+   */
+  onDisplaySuccess(message: string): void {
+    
+    this.snackBar.open(message, 'Close', {
+      duration: 2000
+    });
+
+  }
+
+
+  /**
+   * 
+   */
+  onDisplayWarning(message: string): void {
+    this.snackBar.open(message, 'Close', {
+      duration: 2000
+    });
+
+  }
+
+  /**
+   * 
+   */
+  onDisplayError(message: string): void {
+    this.snackBar.open(message, 'Close', {
+      duration: 2000
+    });
+
+  }
+
 }
