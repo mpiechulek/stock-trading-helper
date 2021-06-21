4dadb64c8810e5fb93be2ef73c9d824838113937
MP <47081376+mpiechulek@users.noreply.github.com>
2021-06-21 15:01:24 +0200

    coppied loading logic form main layout component

--- src/app/layout/auth/auth.component.ts
+++ src/app/layout/auth/auth.component.ts
@@ -1,14 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { Event, NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router } from '@angular/router';
 
 @Component({
   selector: 'app-auth',
-  templateUrl: './auth.component.html'  
+  templateUrl: './auth.component.html'
 })
 export class AuthComponent implements OnInit {
 
-  constructor() { }
+  private showLoadingIndicator: boolean = false;
+
+  constructor(private router: Router) {
+
+    this.router.events.subscribe((routerEvent: Event) => {
+
+      if (routerEvent instanceof NavigationStart) {
+
+        this.showLoadingIndicator = true;
+
+      }
+
+      if (routerEvent instanceof NavigationEnd ||
+        routerEvent instanceof NavigationCancel ||
+        routerEvent instanceof NavigationError
+      ) {
+
+        this.showLoadingIndicator = false;
+
+      }
+
+    })
+
+  }
 
   ngOnInit(): void {
+    
+  }
+
+  /**
+ * 
+ */
+  get getShowLoadingIndicator(): boolean {
+
+    return this.showLoadingIndicator;
+
   }
 
 }
