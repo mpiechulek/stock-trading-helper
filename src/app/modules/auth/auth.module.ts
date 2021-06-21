350f6bad2df175d88ab73abfd8b88eb283482626
MP <47081376+mpiechulek@users.noreply.github.com>
2021-06-21 15:11:38 +0200

    generated components

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
+import { AuthContainerComponent } from './page/auth/auth.container';
+import { AuthComponent } from './components/auth/auth.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AuthContainerComponent,
+    AuthComponent
+  ],
   imports: [
     CommonModule,
     AuthRoutingModule
