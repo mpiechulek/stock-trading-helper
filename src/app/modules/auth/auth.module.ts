d5f0de1d50405255de0ea97d652bd1703ad28109
MP <47081376+mpiechulek@users.noreply.github.com>
2021-06-22 10:31:54 +0200

    removed unecessary component

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -5,13 +5,13 @@ import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './components/auth/login.component';
 import { LoginContainerComponent } from './page/login/login.container';
 import { LoginFormComponent } from './components/auth/login-form/login-form.component';
-import { SideImageComponent } from './components/auth/side-image/side-image.component';
+
 @NgModule({
   declarations: [
     LoginContainerComponent,
     LoginComponent,
     LoginFormComponent,
-    SideImageComponent
+   
   ],
   imports: [
     CommonModule,
