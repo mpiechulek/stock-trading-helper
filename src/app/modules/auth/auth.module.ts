9a9766826f90e2c3c7c97a51b933bc7ec7e90ec1
MP <47081376+mpiechulek@users.noreply.github.com>
2021-06-22 08:27:46 +0200

    generated new components in auth module

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -4,12 +4,16 @@ import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { AuthContainerComponent } from './page/auth/auth.container';
 import { AuthComponent } from './components/auth/auth.component';
+import { LoginFormComponent } from './components/auth/login-form/login-form.component';
+import { SideImageComponent } from './components/auth/side-image/side-image.component';
 
 
 @NgModule({
   declarations: [
     AuthContainerComponent,
-    AuthComponent
+    AuthComponent,
+    LoginFormComponent,
+    SideImageComponent
   ],
   imports: [
     CommonModule,
---
+++ src/app/modules/auth/components/auth/side-image/side-image.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-side-image',
+  templateUrl: './side-image.component.html' 
+})
+export class SideImageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
